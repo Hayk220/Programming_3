597f2b1dd310c69ef5018a84f21b709df5273203
Your Name <you@example.com>
2018-12-15 13:05:11 +0400

    Modules

---
+++ modules/class.bee.js
@@ -0,0 +1,145 @@
+var LivingCreature = require("./class.LivingCreature");
+
+module.exports = class Bee extends LivingCreature {
+    constructor(x, y, index) {
+        super(x, y, index);
+        this.energy = 8;
+
+    }
+
+    getNewCoordinates_1() {
+        this.directions = [
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y - 1],
+            [this.x - 1, this.y - 1],
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x + 2, this.y - 1],
+            [this.x - 2, this.y],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 1],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1],
+            [this.x + 2, this.y + 1],
+            [this.x - 2, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 1, this.y + 2],
+            [this.x + 2, this.y + 2]
+        ];
+    }
+
+    getNewCoordinates_2() {
+        this.directions = [
+            [this.x - 2, this.y - 2],
+            [this.x - 1, this.y - 2],
+            [this.x, this.y - 2],
+            [this.x + 1, this.y - 2],
+            [this.x + 2, this.y - 2],
+            [this.x - 2, this.y - 1],
+            [this.x + 2, this.y - 1],
+            [this.x - 2, this.y],
+            [this.x + 2, this.y],
+            [this.x - 2, this.y + 1],
+            [this.x + 2, this.y + 1],
+            [this.x - 2, this.y + 2],
+            [this.x - 1, this.y + 2],
+            [this.x, this.y + 2],
+            [this.x + 1, this.y + 2],
+            [this.x + 2, this.y + 2]
+        ];
+    }
+
+    chooseCell_1(num) {
+        this.getNewCoordinates_1();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push([x, y]);
+                }
+                else if (matrix[y][x].index == num) {
+                    found.push([x, y]);
+                }
+            }
+        }
+        return found;
+    }
+
+    chooseCell_2(num) {
+        this.getNewCoordinates_2();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push([x, y]);
+                }
+                else if (matrix[y][x].index == num) {
+                    found.push([x, y]);
+                }
+
+            }
+        }
+        return found;
+    }
+
+    move() {
+        if (this.acted == false) {
+
+            var newCell = random(this.chooseCell_2(0));
+            if (newCell) {
+                var newX = newCell[0];
+                var newY = newCell[1];
+
+                matrix[newY][newX] = matrix[this.y][this.x];
+                matrix[this.y][this.x] = 0;
+
+                this.x = newX;
+                this.y = newY;
+                this.acted = true;
+                this.energy--;
+                if (this.energy <= 0) {
+                    this.die();
+                }
+
+            }
+        }
+
+    }
+
+    eat() {
+        if (this.acted == false) {
+
+            var newCell = random(this.chooseCell_1(3));
+            if (newCell) {
+                var newX = newCell[0];
+                var newY = newCell[1];
+                matrix[newY][newX].energy -= 3;
+                this.acted == true;
+                this.energy++;
+            }
+            else {
+                this.move();
+            }
+        }
+
+
+    }
+
+    die() {
+        matrix[this.y][this.x] = 0;
+    }
+
+}
---
+++ modules/class.mushroom.js
@@ -0,0 +1,48 @@
+var LivingCreature = require("./class.LivingCreature");
+
+module.exports = class Sunk extends LivingCreature {
+    constructor(x, y, index) {
+        super(x, y, index)
+        this.multiply = 0;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+
+    }
+    chooseCell(num) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == num) {
+                    found.push([x, y]);
+                }
+            }
+        }
+        return found;
+    }
+
+    mul() {
+        this.multiply++;
+        var newCell = random(this.chooseCell(0));
+
+        if (newCell && this.multiply >= 10) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[newY][newX] = new Sunk(newX, newY, 4);
+            this.multiply = 0;
+
+        }
+    }
+
+
+}
